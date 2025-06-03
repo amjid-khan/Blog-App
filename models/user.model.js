0fbade79302d2a656f964204a1c0ef0c9fcd075c
Amjid khan <amjidkurrmywal@gmail.com>
2025-06-03 09:42:20 +0500

    add user model

---
+++ models/user.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import { createHmac, randomBytes } from "crypto"
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    salt: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profileImage: {
+        type: String,
+        default: "/images/default.png"
+    },
+    role: {
+        type: String,
+        enum: ["USER", "ADMIN"],
+        default: "USER"
+    }
+}, { timestamps: true })
+
+userSchema.pre("save", function (next) {
+    const user = this;
+    if (!user.isModified("password")) return;
+    const salt = randomBytes(16).toString()
+    const hashedPassword = createHmac("sha256", salt).update(user.password).digest("hex")
+    this.salt = salt;
+    this.password = hashedPassword
+    next()
+})
+
+const User = mongoose.model("User", userSchema)
+
+export default User
