3b1f5b56c68efa40aedffd6f73ff3981dd862c4e
Amjid khan <amjidkurrmywal@gmail.com>
2025-06-03 09:25:53 +0500

    fix server bugs and add homepage from ejs

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ import path from "path"
 import dotenv from "dotenv"
 dotenv.config()
 
-app.set("view engine", "ejs")
-app.set("view", path.resolve("./views"))
-
 const app = express()
+
+app.set("view engine", "ejs")
+app.set("views", path.resolve("./views"))
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is ready on port : ${process.env.PORT}`)
