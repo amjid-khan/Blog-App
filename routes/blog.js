26642957fa4574b81308c49eab8880db9a933267
Amjid khan <amjidkurrmywal@gmail.com>
2025-06-04 11:24:07 +0500

    install multer and save files in multer

--- routes/blog.js
+++ routes/blog.js
@@ -1,5 +1,20 @@
-
+import express from "express"
+import multer from "multer"
+import path from "path"
 const blogRouter = express.Router()
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.resolve(`./public/upload`))
+    },
+    filename: function (req, file, cb) {
+        const fileName = `${Date.now()}-${file.originalname}`
+        cb(null, fileName)
+    }
+})
+
+const upload = multer({ storage: storage })
+
 
 blogRouter.get("/add-new", (req, res) => {
     return res.render("addBlog", {
@@ -7,8 +22,9 @@ blogRouter.get("/add-new", (req, res) => {
     })
 })
 
-blogRouter.post("/add-new", (req, res) => {
+blogRouter.post("/add-new", upload.single("coverImage"), (req, res) => {
     console.log(req.body)
+    console.log(req.file)
     return res.redirect("/",)
 })
 
